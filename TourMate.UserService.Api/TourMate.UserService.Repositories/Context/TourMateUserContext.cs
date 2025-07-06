c22f4349d00e230c9b0694951207655e693c63d2
mineduck1608 <ducdm160804@gmail.com>
2025-07-06 14:31:35 +0700

    Update TourMateUserContext.cs

--- TourMate.UserService.Api/TourMate.UserService.Repositories/Context/TourMateUserContext.cs
+++ TourMate.UserService.Api/TourMate.UserService.Repositories/Context/TourMateUserContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 using TourMate.UserService.Repositories.Models;
 
 namespace TourMate.UserService.Repositories.Context;
@@ -22,9 +23,18 @@ public partial class TourMateUserContext : DbContext
 
     public virtual DbSet<TourGuideDesc> TourGuideDescs { get; set; }
 
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=MINEDUCK\\MINEDUCK;Initial Catalog=TourMate_User;Persist Security Info=False;User ID=sa;Password=12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+       => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection"));
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
