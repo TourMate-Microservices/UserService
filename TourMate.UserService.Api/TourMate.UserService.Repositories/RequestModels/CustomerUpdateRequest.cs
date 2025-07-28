3d49a93468470a6d024a3dde25ff13dd2c170c18
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-07-28 16:05:28 +0700

    Up

---
+++ TourMate.UserService.Api/TourMate.UserService.Repositories/RequestModels/CustomerUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourMate.UserService.Repositories.RequestModels
+{
+    public class CustomerUpdateRequest
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Gender { get; set; }
+    }
+}
